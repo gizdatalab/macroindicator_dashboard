94cba3eb29526b3313d25fd68f749de0d840eeb0
Lea Voigt <leavoigt97@gmail.com>
2023-09-27 14:43:44 +0200

    delete prod params file

--- prod_get_data.py
+++ prod_get_data.py
@@ -1,10 +1,32 @@
 import pandas as pd 
-
 from api_functions.wb_data import get_wb_data
 from api_functions.imf_data import get_imf_data
 
-from prod_params import START_YEAR, END_YEAR, featureMap_indicators_wb, featureMap_indicators_imf, DATASET
+########################### SPECIFY START AND END YEAR ###############################
 
+# IMF only has data until 2017 (as of August 2023) and data retrievel doesn't work if 
+# end_year > 2017 
+
+START_YEAR = 2000
+END_YEAR = 2017
+
+########################### SPECIFY THE WB INDICATORS NEEDED ##########################
+
+featureMap_indicators_wb={
+    'NY.GDP.PCAP.PP.KD': 'GDP per capita',
+    'NY.GDP.MKTP.PP.KD': 'GDP',
+    'SP.POP.TOTL': 'Total population',
+    'SP.POP.GROW': 'Population Growth Rate'}
+
+########################### SPECIFY THE IMF INDICATORS NEEDED #########################
+
+featureMap_indicators_imf = {
+    'rnna': 'Capital stock (in bil. 2011US$)',
+    'rnna_pch': 'Growth rate in total capital (%)'
+}
+
+# Dataset used (currently only works for one dataset at a time)
+DATASET = "PGCS"
 
 ########################### RETRIEVE THE DATA ##########################
 
